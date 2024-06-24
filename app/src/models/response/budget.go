38750d3b495d32f9a553eda06b4da7b448bcc50e
luis <luisgerarhern@outlook.com>
2024-06-24 11:38:06 -0600

    refactor: response Budget

--- app/src/models/response/budget.go
+++ app/src/models/response/budget.go
@@ -3,13 +3,13 @@ package responseModel
 import "time"
 
 type Budget struct {
+	ID              int64     `json:"id"`
+	Name            string    `json:"name"`
 	TotalBudget     float32   `json:"totalBudget"`
-	UsedBudget      float32   `json:"usedBudget"`
 	RemainingBudget float32   `json:"remainigBudget"`
-	Assets          []Record  `json:"assets"`
-	Expenses        []Record  `json:"expenses"`
+	UsedBudget      float32   `json:"usedBudget"`
 	StartTime       time.Time `json:"starttime"`
 	EndTime         time.Time `json:"endtime"`
-	Name            string    `json:"name"`
-	ID              int64     `json:"id"`
+	Assets          []Record  `json:"assets"`
+	Expenses        []Record  `json:"expenses"`
 }
