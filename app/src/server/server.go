bfee25e1dab822e6bbdcd0130a0e4cc1848d86ea
luis <luisgerarhern@outlook.com>
2024-07-02 20:24:38 -0600

    hotfix: added missing cors

--- app/src/server/server.go
+++ app/src/server/server.go
@@ -2,11 +2,17 @@ package server
 
 import (
 	"github.com/LuisGerardoDC/personalFinances/app/src/server/paths"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	router := gin.Default()
+	config := cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}
+	router.Use(cors.New(config))
 	paths.AddBudgetRoutes(router)
 	return router
 }
