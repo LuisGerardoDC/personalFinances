658d1740144df96b37d4811881675434c36ce7d5
luis <luisgerarhern@outlook.com>
2024-07-24 18:39:08 -0600

    Dockerized (first atempt)

--- app/src/utils/db_connect.go
+++ app/src/utils/db_connect.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,7 +12,7 @@ func ConnectDB() *sql.DB {
 
 	usuario := "personalFinancesBack"
 	pass := "Yolu@54&loki"
-	host := "localhost:3306"
+	host := fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	dbName := "personal_finances"
 
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", usuario, pass, host, dbName))
